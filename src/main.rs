4d0d37406b1f2b9e9f7683d967c2a8c40ab43d59
dmathieu <damien.mathieu@auth0.com>
2021-05-31 17:13:17 +0200

    setup Shelly struct

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,5 @@
+mod shelly;
+
 fn main() {
     println!("Hello, world!");
 }
---
+++ src/shelly.rs
@@ -0,0 +1,37 @@
+use reqwest::Client;
+use serde::Deserialize;
+
+#[derive(Debug, Deserialize)]
+pub struct Shelly {
+    pub uptime: u64,
+}
+
+impl Shelly {
+    pub async fn load(url: String) -> Result<Shelly, Box<dyn std::error::Error>> {
+        let resp = Client::new().get(url).send().await?;
+        let shelly = resp.json::<Shelly>().await?;
+        Ok(shelly)
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use httptest::{matchers::*, responders::*, Expectation, Server};
+    use std::fs;
+
+    #[tokio::test]
+    async fn test_shelly_load() {
+        let content = fs::read_to_string("fixtures/plug.json").unwrap();
+        let server = Server::run();
+        server.expect(
+            Expectation::matching(request::method_path("GET", "/shelly"))
+                .respond_with(status_code(200).body(content)),
+        );
+
+        let shelly = Shelly::load(server.url("/shelly").to_string())
+            .await
+            .unwrap();
+        assert_eq!(343771, shelly.uptime);
+    }
+}
