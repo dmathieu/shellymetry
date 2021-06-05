7447c32bc7fb190e5ea002c552a416495830c636
dmathieu <damien.mathieu@auth0.com>
2021-06-05 17:44:43 +0200

    add devices to config

--- src/config.rs
+++ src/config.rs
@@ -1,9 +1,18 @@
 use config;
 use serde::Deserialize;
+use std::collections::HashMap;
+
+#[derive(Debug, Deserialize, PartialEq)]
+pub struct Device {
+    pub kind: String,
+    pub name: String,
+    pub labels: HashMap<String, String>,
+}
 
 #[derive(Debug, Deserialize)]
 pub struct Config {
     pub server_port: u16,
+    pub devices: Vec<Device>,
 }
 
 pub fn build(config_path: String) -> Result<Config, config::ConfigError> {
@@ -22,5 +31,16 @@ mod tests {
     fn test_config_build() {
         let config = build("fixtures/config.json".to_string()).unwrap();
         assert_eq!(1304, config.server_port);
+
+        let mut labels = HashMap::new();
+        labels.insert("room".to_string(), "bedroom".to_string());
+        assert_eq!(
+            vec![Device {
+                kind: "plug".to_string(),
+                name: "ABCDEF".to_string(),
+                labels: labels,
+            }],
+            config.devices
+        );
     }
 }
